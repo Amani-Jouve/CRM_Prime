288acd19fbff865af822bf22a44679ad2fed4737
Amani Jouve <amani.jouve@gmail.com>
2022-06-09 09:32:46 +0200

    Version initiale

---
+++ Main/models.py
@@ -0,0 +1,156 @@
+from django.db import models
+from django.utils import timezone
+
+
+
+class Customer(models.Model):
+    """docstring fos Customer"""
+    
+    GENDER_CHOICES=(('Masculin','Masculin'),('Féminin','Féminin'),('Autre','Autre')
+    )
+    
+    name=models.CharField(max_length=255, null=True)
+    customer_type=models.CharField(max_length=255, null=True)
+    DoB=models.DateTimeField(null=True)
+    gender=models.CharField(max_length=255, null=True,choices=GENDER_CHOICES)
+    email=models.CharField(max_length=255, null=True) 
+    phone=models.CharField(max_length=12, null=True)
+    address=models.CharField(max_length=255, null=True)
+    town=models.CharField(max_length=255, null=True)
+    region=models.CharField(max_length=255, null=True)
+    date_created=models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__(self):
+        return self.name
+
+    @property
+    def nb_orders(self):
+        my_orders=Order.objects.filter(customer__id=self.id)
+        return my_orders.count()
+    
+    @property
+    def total_orders(self):
+        my_orders=Order.objects.filter(customer__id=self.id)
+        total=0
+        for item in my_orders:
+            total=total+item.get_total_order_price_TTC
+        return total
+    
+    @property
+    def segment(self):
+        if self.total_orders>1000:
+            segmentation="Bon client"
+        else:
+            segmentation="Client ponctuel"
+        return segmentation
+
+class Product(models.Model):
+    """docstring fos Product"""
+    
+    CATEGORY_CHOICES=(('PC','PC'),('Autre','Autre'))
+    
+    name=models.CharField(max_length=200, null=True)
+    description=models.CharField(max_length=200, null=True)
+    category=models.CharField(max_length=200, null=True,choices=CATEGORY_CHOICES)
+    n_lot=models.CharField(max_length=200, null=True)
+    price_pdt_HT=models.FloatField(null=True)
+    price_pdt_TTC=models.FloatField(null=True)
+    commercial_margin=models.FloatField(null=True)
+    stock_q=models.IntegerField(null=True)
+    stock_security=models.IntegerField(null=True)
+    
+    
+    def __str__(self):
+        return self.name    
+    
+    @property
+    def stock_q_actuel(self):
+        my_orders=Order.objects.filter(product__id=self.id)
+        return self.stock_q-my_orders.count()
+        
+        
+    @property
+    def stock_status(self):#for particular product order total
+        if self.stock_q_actuel < self.stock_security:
+            stock_res="A réapprovisionner"
+        else:
+            stock_res="Okay"
+        return stock_res
+    
+    
+    
+class Order(models.Model):  
+    """docstring fos Order"""
+    
+    STATUS_CHOICES=(('en préparation','en préparation'),('expédié','expédié'),('livré','livré'),('retour client','retour client'))
+    
+    customer=models.ForeignKey(Customer,null=True, on_delete=models.SET_NULL)
+    product=models.ForeignKey(Product,null=True, on_delete=models.SET_NULL)
+    
+    quantity=models.FloatField(null=True)
+    discount=models.FloatField(blank=True,null=True)
+    date_created=models.DateTimeField(auto_now_add=True, null=True)
+    
+    status=models.CharField(max_length=255, null=True,choices=STATUS_CHOICES)
+    Delivery_date_expected=models.DateTimeField(null=True)
+    Delivery_date_final=models.DateTimeField(blank=True, null=True)                   
+
+    @property
+    def get_total_item_price_HT(self):#for particular product order total
+        return self.quantity * self.product.price_pdt_HT*(1-self.discount)
+    
+    @property
+    def get_total_item_price_TTC(self):#for particular product order total
+        return self.quantity * self.product.price_pdt_TTC*(1-self.discount)
+    
+    @property
+    def late_delivery(self):
+        today=timezone.now()
+        if self.Delivery_date_expected < today:
+            delivery_res="En retard"
+        else:
+            delivery_res="Dans les temps"
+        return delivery_res
+    
+    @property
+    def delivery_fees(self):
+        if self.get_total_item_price_HT<2000:
+            fees_res=10
+        else:
+            fees_res=0
+        return fees_res
+    
+    @property
+    def get_total_order_price_TTC(self):#for particular product order total
+        return self.get_total_item_price_TTC+self.delivery_fees
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ Main/views.py
@@ -0,0 +1,98 @@
+from django.shortcuts import render
+from .models import Customer,Product,Order
+
+# Pages / vues principales 
+
+def home(request):
+    return render(request,'Main/home.html')
+
+def customers(request):
+    
+    if request.method=="POST":
+        customer=Customer()
+        customer.name=request.POST['customer_name']
+        customer.customer_type=request.POST['customer_type']
+        customer.DoB=request.POST['customer_DoB']
+        customer.gender=request.POST['customer_gender']
+        customer.email=request.POST['customer_email']
+        customer.phone=request.POST['customer_phone']
+        customer.address=request.POST['customer_address']
+        customer.town=request.POST['customer_town']
+        customer.region=request.POST['customer_region']
+        customer.save()
+        return redirect('/')
+    else:
+        my_customer=Customer.objects.all()
+        
+        context={"my_customer": my_customer}
+        return render(request,'Main/customers.html',context)
+
+def products(request):
+    if request.method=="POST":
+        product=Product()
+        product.name=request.POST['product_name']
+        product.description=request.POST['product_description']
+        product.category=request.POST['product_category']
+        product.n_lot=request.POST['product_n_lot']
+        product.price_pdt_HT=request.POST['product_price_pdt_HT']
+        product.price_pdt_TTC=request.POST['product_price_pdt_TTC']
+        product.commercial_margin=request.POST['product_commercial_margin']
+        product.stock_q=request.POST['product_stock_q']
+        product.stock_security=request.POST['product_stock_security']
+        product.stock_status=request.POST['product_stock_status']
+        product.save()
+        return redirect('/')
+    else:
+        my_product=Product.objects.all()
+        context={"my_product": my_product}
+        return render(request,'Main/products.html',context)
+
+def orders(request):
+    if request.method=="POST":
+        order=Order()
+        order.customer=request.POST['order_customer']
+        order.product=request.POST['order_product']
+        order.total_price_HT=request.POST['order_total_price_HT']
+        order.total_price_TTC=request.POST['order_total_price_TTC']
+        order.status=request.POST['order_status']
+        order.Delivery_date_expected=request.POST['order_Delivery_date_expected']
+        order.Delivery_date_final=request.POST['order_Delivery_date_final']
+        order.save()
+        return redirect('/')
+    else:
+        my_order=Order.objects.all()
+        context={"my_order": my_order}
+        return render(request,'Main/orders.html',context)
+
+# Pages / vues - Création
+
+def create_customers(request):
+    return render(request,'Main/customers_form.html')
+
+def create_products(request):
+    return render(request,'Main/products_form.html')
+
+def create_orders(request):
+    return render(request,'Main/orders_form.html')
+
+# Pages / vues - Update
+
+def update_customers(request):
+    return render(request,'Main/customers_form.html')
+
+def update_products(request):
+    return render(request,'Main/products_form.html')
+
+def update_orders(request):
+    return render(request,'Main/orders_form.html')
+
+# Pages / vues - Delete
+
+def delete_customers(request):
+    return render(request,'Main/customers_form.html')
+
+def delete_products(request):
+    return render(request,'Main/products_form.html')
+
+def delete_orders(request):
+    return render(request,'Main/orders_form.html')
